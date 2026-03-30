7af38749516b9b741670eae8593553c93bae25eb
yaming080 <yaming080@gmail.com>
2026-03-31 05:07:58 +0900

    Update doorinews_bot.py

--- doorinews_bot.py
+++ doorinews_bot.py
@@ -1595,8 +1595,8 @@ def build_message(story: dict) -> str:
             title=title,
             max_sentences=SUMMARY_SENTENCES
         )
-        summary_ko = translate_text_to_korean(raw_summary)
-        summary_ko = cleanup_text(summary_ko)
+        entities = []
+        dynamic_tags = []
         summary_ko = fix_translation_terms(summary_ko)
         summary_ko = fix_truncated_phrases(summary_ko)
         summary_ko = normalize_style(summary_ko)
